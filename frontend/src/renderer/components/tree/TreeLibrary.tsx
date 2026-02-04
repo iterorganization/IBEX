e402885fab4d6d5400bdb7db94fb11bc6ac42e0e
Maël Fiaudrin <mfiaudrin@epsyl-alcen.com>
2026-02-04 16:08:32 +0100

    add icon in tree for complex type

--- frontend/src/renderer/components/tree/TreeLibrary.tsx
+++ frontend/src/renderer/components/tree/TreeLibrary.tsx
@@ -11,6 +11,7 @@ import {
 } from '@mantine/core';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import {
+  IconBraces,
   IconFileUnknown,
   IconFolder,
   IconFolderOpen,
@@ -192,6 +193,7 @@ function NodeIcon({
           NodeInfoTypeEnum.INTEGER,
           NodeInfoTypeEnum.FLOAT,
           NodeInfoTypeEnum.STRING,
+          NodeInfoTypeEnum.COMPLEX,
         ].includes(type)
       ) {
         if (hasUserSelectedText()) {
@@ -308,6 +310,9 @@ function NodeIcon({
       ),
       [NodeInfoTypeEnum.STRING]: getCheckboxIcon(
         <IconTypography {...commonProps} className={classes.forcedWidth} />,
+      ),
+      [NodeInfoTypeEnum.COMPLEX]: getCheckboxIcon(
+        <IconBraces {...commonProps} className={classes.forcedWidth} />,
       ),
     };
 
