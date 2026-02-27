ba9a7239d23b8ebdaa5901bd5b06e0d0e369ec70
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2026-02-27 16:45:58 +0100

    fixed ci script and reverted asv config

--- backend/ci/configure_env.sh
+++ backend/ci/configure_env.sh
@@ -7,7 +7,8 @@ source /etc/profile.d/modules.sh
 module purge
 
 # Set up environment
-module load IDStools/2.1.0-intel-2023b IMAS-Python/2.0.0-intel-2023b
+module load IDStools/2.4.0-intel-2023b
+module unload Python-bundle-PyPI
 
 # Debuggging:
 echo "Done loading modules"
