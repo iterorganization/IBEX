8ccc3d004ccd7b93693bf4b497ada889122ad8d0
wasikj <jwasik@man.poznan.pl>
2026-03-31 17:27:14 +0200

    Apply linting to tests

--- backend/tests/test_data_interpolation.py
+++ backend/tests/test_data_interpolation.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pytest
 
+
 def test_simple_interpolation(interpolation_entry_path_directory):
 
-    db_names = [f"imas:hdf5?path={interpolation_entry_path_directory}/interpolation_db_1",f"imas:hdf5?path={interpolation_entry_path_directory}/interpolation_db_2"]
+    db_names = [
+        f"imas:hdf5?path={interpolation_entry_path_directory}/interpolation_db_1",
+        f"imas:hdf5?path={interpolation_entry_path_directory}/interpolation_db_2",
+    ]
     uri_fragment = "#equilibrium/time_slice[:]/profiles_2d[:]/psi"
-    parameters = {
-        "uri": f"{db_names[0]}/{uri_fragment}",
-        "interpolate_over" : [f"{db_names[1]}/{uri_fragment}"]
-    }
+    parameters = {"uri": f"{db_names[0]}/{uri_fragment}", "interpolate_over": [f"{db_names[1]}/{uri_fragment}"]}
     response = pytest.test_client.get("/data/plot_data", params=parameters)
 
     assert response.status_code == 200
@@ -16,6 +17,5 @@ def test_simple_interpolation(interpolation_entry_path_directory):
     json_data = response.json()["data"]
     coords = json_data["coordinates"]
 
-    coord_shapes = [list(np.asarray(c['value']).shape) for c in coords]
-    assert coord_shapes == [[4,4,12],[4,4,3],[4,4],[4]]
-
+    coord_shapes = [list(np.asarray(c["value"]).shape) for c in coords]
+    assert coord_shapes == [[4, 4, 12], [4, 4, 3], [4, 4], [4]]
